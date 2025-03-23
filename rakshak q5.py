530993f3fde691fa7135e416360749362bcbc02a
Mahaswi <tanmaimaha0903@gmail.com>
2025-03-23 20:53:45 +0530

    Submission

--- rakshak q5.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x=np.linspace(-10, 10 , 400)
-y=x**2
-
-xscatter=np.random.uniform(0, 1, 100)
-yscatter=np.random.normal(0.5 ,0.1 ,100)
-
-exp=np.random.exponential(1, 500)
